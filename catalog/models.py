cae3344babd19e93d1fbbdf19a961f3991907d69
Konstantin Sobolev <comediant683@mail.ru>
2024-05-13 16:55:39 +0300

    home work 20.1 1st commit(zadaniy 1-3,5)

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Наименование")
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Наименование")
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    image = models.ImageField(upload_to='catalog/', verbose_name='Изображение (превью)', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена за покупку')
+    created_at = models.DateTimeField(default=timezone.now, verbose_name='Дата создания (записи в БД)')
+    updated_at = models.DateTimeField(default=timezone.now, verbose_name='Дата последнего изменения (записи в БД)')
+
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
--- config/settings.py
+++ config/settings.py
@@ -60,8 +60,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "BASE_HOME",
+        'USER': "postgres",
+        'PASSWORD': "1234",
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
@@ -88,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
